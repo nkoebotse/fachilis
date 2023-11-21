d5281ec7f3de7d6f9c23895dafac97ba682d09c0
Nkoebotse Elliot Sekgobela <136261850+nkoebotse@users.noreply.github.com>
2023-11-21 11:10:28 +0000

    code README.md

--- app.py
+++ app.py
@@ -1,10 +1,59 @@
-from flask import Flask, render_template
+# app.py
+
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__, static_folder='static')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
 
 @app.route('/')
 def login():
     return render_template('login.html')
 
+@app.route('/forgot_password')
+def forgot_password():
+    return render_template('forgot_password.html')
+
+@app.route('/create_account', methods=['GET', 'POST'])
+def create_account():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+
+        if password != confirm_password:
+            error = 'Passwords do not match'
+            return render_template('create_account.html', error=error)
+
+        hashed_password = generate_password_hash(password, method='sha256')
+
+        new_user = User(username=username, email=email, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for('login'))
+
+    return render_template('create_account.html')
+
+@app.route('/menu', methods=['GET', 'POST'])
+def menu():
+    if request.method == 'POST':
+        # Handle order submission
+        # Update order summary, calculate total, etc.
+        pass
+
+    return render_template('menu.html')
+
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
